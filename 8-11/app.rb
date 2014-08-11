f419f5e7af98437f68853be8e5287ded0291c9ed
Andrew House <andrewhouse621@gmail.com>
2014-08-11 15:35:53 -0400

    homework 8-11

---
+++ 8-11/app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'httparty'
+require './github.rb'
+
+get '/' do
+  haml :index
+end
+
+get '/times' do
+  @max ||= 10
+  @min ||= 1
+  haml :table
+end
+
+post '/times' do
+  @max = params[:max].to_i
+  @min = params[:min].to_i
+  haml :table
+end
+
+get '/letters' do
+  haml :letter_count
+end
+
+post '/letters' do
+  @words = params[:word]
+  @number_of_letters = @words.gsub(" ", "").split("").inject(Hash.new(0)) do |hash,letter|
+      hash[letter] += 1
+      hash
+    end
+  haml :letter_count
+end
+
+get '/repos' do
+  haml :repos
+end
+
+post '/repos' do
+  @name = params[:github_name]
+  @repos = Github.get("https://api.github.com/users/#{@name}/repos")
+  haml :repos
+end
